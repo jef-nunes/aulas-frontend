2914492af3412a4c9170fce1618eb24bf96281d9
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-09-24 16:36:06 -0300

    Update index.js

--- relogio/index.js
+++ relogio/index.js
@@ -1,28 +1,94 @@
-class Clock{
+class Clock
+{
+    tagDay;
+    tagMonth;
+    tagYear;
+    tagGreetings;
     tagHours;
     tagMinutes;
     tagSeconds;
 
-    constructor(){
+    constructor()
+    {
+        this.tagDay = document.getElementById("clock-day");
+        this.tagMonth = document.getElementById("clock-month");
+        this.tagYear = document.getElementById("clock-year");
+        this.tagGreetings = document.getElementById("clock-greetings");
         this.tagHours = document.getElementById("clock-hours");
         this.tagMinutes = document.getElementById("clock-minutes");
         this.tagSeconds = document.getElementById("clock-seconds");
     }
 
-    updateTime = () => {
+    setDate()
+    {
         let date = new Date();
-        this.tagHours.textContent = date.getHours();
-        this.tagMinutes.textContent = date.getMinutes();
-        this.tagSeconds.textContent = date.getSeconds();
+        let day = date.getDate();
+        let month = date.getMonth()+1;
+        let year = date.getFullYear();
+        this.tagDay.textContent = day+"/";
+        
+        if(month<10)
+        this.tagMonth.textContent = `0${month}/`;
+        else
+        this.tagMonth.textContent = month+"/";
+
+        this.tagYear.textContent = year;
     }
 
-    activate(){
+    setGreetings()
+    {
+        let date = new Date();
+        let hours = date.getHours();
+        if(hours >= 5 && hours <= 12)
+        {
+            this.tagGreetings.textContent = "Bom Dia";
+        }
+        else if(hours >= 12 && hours <= 18)
+        {
+            this.tagGreetings.textContent = "Boa Tarde";
+        }
+        else
+        {
+            this.tagGreetings.textContent = "Boa Noite";
+        }
+    }
+
+    updateTime()
+    {
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+
+        if(hours<10)
+        this.tagHours.textContent = `0${hours}`;
+        else
+        this.tagHours.textContent = hours;
+
+        if(minutes<10)
+        this.tagMinutes.textContent = `0${minutes}`;
+        else
+        this.tagMinutes.textContent = minutes;
+
+        if(seconds<10)
+        this.tagSeconds.textContent = `0${seconds}`;
+        else
+        this.tagSeconds.textContent = seconds;
+    }
+
+    activate()
+    {
         this.updateTime();
-        setInterval(this.updateTime, 1000);
+        this.setDate();
+        this.setGreetings();
+        setInterval(() => this.updateTime(), 1000);
     }
 }
 
-function main(){
+
+
+function main()
+{
     const clock = new Clock();
     clock.activate();
 }
