64e7808a1cc86161c56f136ec7903a6223e127fa
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-12-09 10:58:42 -0300

    Add files via upload

---
+++ jogo-jump/index.js
@@ -0,0 +1,36 @@
+const jogador = document
+.querySelector(".jogador");
+
+const projetil = document
+.querySelector(".projetil");
+
+
+function jogadorPular()
+{
+    jogador.classList.add("jogador-pulo");
+    setTimeout(()=>{
+        jogador.classList.remove("jogador-pulo");
+    },500);
+}
+
+const loop = setInterval(()=>{
+    const projetilPos = projetil.offsetLeft;
+    
+    const jogadorPos = +window
+    .getComputedStyle(jogador)
+    .bottom
+    .replace("px","");
+
+    if(projetilPos <= 170 &&
+        projetilPos > 0 &&
+        jogadorPos<100){
+        projetil.style.display = "none";
+        projetil.style.animation = "none";
+        projetil.style.animation = `${projetilPos}px`;
+        jogador.style.animation = "none"
+        jogador.style.bottom = `${jogadorPos}px`;
+        jogador.src="./images/megaman-derrotado.png";
+    }
+}, 300);
+
+document.addEventListener("keydown", jogadorPular);
